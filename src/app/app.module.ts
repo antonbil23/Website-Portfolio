8a9ab4ca078a4d0032697359b348896e5157433a
antonbil23 <anbilonog@gmail.com>
2023-05-14 16:11:53 -0400

    More changes
    I added some components so that I can add different pages for the header sections.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,18 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
 import { ExperienceComponent } from './experience/experience.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'home', component: WelcomePageComponent},
+  { path: 'about', component: AboutComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'contact', component: ContactComponent },
+  // Add more routes as needed
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +26,9 @@ import { ExperienceComponent } from './experience/experience.component';
     WelcomePageComponent,
     HeaderComponent,
     AboutComponent,
-    ExperienceComponent
+    ExperienceComponent,
+    ContactComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
